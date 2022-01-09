f81374ddf5211b4531631250f0eb2288d202ba9c
DanKneeD <danieldousek@gmail.com>
2022-01-09 14:37:16 -0500

    TestCommit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing, Please Dont Break.
         </a>
       </header>
     </div>
